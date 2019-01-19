5fc5fce52b5a5b061a18d90593c714a1cc8a208e
Pat Sweeney <patricsw@microsoft.com>
2019-01-19 11:03:59 -0800

    Simple app for reading barcodes and writing them to a .csv file

---
+++ Tools/UPCInput/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+
+namespace UPCInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> items = new Dictionary<string, int>();
+            while (true)
+            {
+                System.Console.WriteLine("UPC:");
+                string s = System.Console.ReadLine();
+                if (s == "762274813938")
+                    break;
+                if (items.ContainsKey(s))
+                    items[s]++;
+                else
+                    items.Add(s, 1);
+            }
+            // write data to csv
+            FileStream fs = new FileStream(@"d:\inventory.csv", FileMode.Create);
+            StreamWriter strm = new StreamWriter(fs);
+            foreach (var data in items)
+            {
+                strm.WriteLine($"{data.Key},{data.Value.ToString()}");
+            }
+            strm.Flush();
+            fs.Close();
+        }
+    }
+}
